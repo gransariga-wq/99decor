4bd362cc61e2b477b78ef1afce1b296273060527
Salman Khan <salman250195@gmail.com>
2025-12-20 23:27:46 +0530

    fixed map issues

--- src/app/pages/Contact/contact-1/contact-1.component.ts
+++ src/app/pages/Contact/contact-1/contact-1.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+declare var window: any;
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'assets/images/marker-icon-2x.png',
+  iconUrl: 'assets/images/marker-icon.png',
+  shadowUrl: 'assets/images/marker-shadow.png'
+});
 
 @Component({
   selector: 'app-contact-1',
   templateUrl: './contact-1.component.html',
   styleUrls: ['./contact-1.component.css']
 })
-export class Contact1Component {
+export class Contact1Component implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = L.map('map_canvas', {
+      // center: [19.0760, 72.8777], // Mumbai
+      center: [19.1176, 72.9060], // 📍 Powai, Mumbai
+      zoom: 12,
+      scrollWheelZoom: false
+    });
+
+    L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+      attribution: ''
+    }).addTo(map);
+
+    // L.marker([19.0760, 72.8777])
+    //   .addTo(map)
+    //   .bindPopup('<b>Mumbai</b><br>India')
+    //   .openPopup();
+
+    L.marker([19.0760, 72.8777])
+      .addTo(map)
+      .bindPopup('Pawai, Mumbai')
+      .openPopup();
+  }
 
 }
--- src/app/pages/Contact/contact-2/contact-2.component.ts
+++ src/app/pages/Contact/contact-2/contact-2.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+declare var window: any;
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 @Component({
   selector: 'app-contact-2',
   templateUrl: './contact-2.component.html',
   styleUrls: ['./contact-2.component.css']
 })
-export class Contact2Component {
+export class Contact2Component implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+      const map = L.map('map_canvas', {
+            // center: [19.0760, 72.8777], // Mumbai
+            center: [19.1176, 72.9060], // 📍 Powai, Mumbai
+            zoom: 12,
+            scrollWheelZoom: false
+          });
+      
+          L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+            attribution: ''
+          }).addTo(map);
+      
+          // L.marker([19.0760, 72.8777])
+          //   .addTo(map)
+          //   .bindPopup('<b>Mumbai</b><br>India')
+          //   .openPopup();
+      
+          L.marker([19.0760, 72.8777])
+            .addTo(map)
+            .bindPopup('Pawai, Mumbai')
+            .openPopup();
+  }
 
 }
